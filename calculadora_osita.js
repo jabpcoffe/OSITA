a52076e8b7aa78002fcf16856b2f5ba5c9ca7f50
jabpcoffe <barretopadrino@gmail.com>
2025-07-12 21:05:54 -0400

    Delete calculadora_osita.js

--- calculadora_osita.js
+++
@@ -1,6 +0,0 @@
-let display = document.getElementById("display");
-let sound = new Audio("click-kawaii.mp3");
-function playSound(){sound.currentTime=0;sound.play();}
-function appendValue(value){playSound();display.value+=value;}
-function clearDisplay(){playSound();display.value="";}
-function calculateResult(){playSound();try{display.value=eval(display.value);}catch(e){display.value="Error";}}
